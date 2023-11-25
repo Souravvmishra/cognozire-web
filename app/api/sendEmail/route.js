7a6965f679f2f527edbae31404868ee18b97c915
souravvmishra <souravvmishra@gmail.com>
2023-11-25 23:47:39 +0530

    changed to resend

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,19 +1,9 @@
 import { render } from '@react-email/render';
 import EmailTemplate from '@/templates/EmailTemplate';
 import { NextResponse } from "next/server";
-import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 
-
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com', // Hostinger's SMTP server hostname
-    port: 587, // Hostinger's recommended SMTP port
-    secure: false, // Hostinger typically uses TLS, so set to true if necessary
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD // Your email password (consider using an app-specific password)
-    },
-  });
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 export async function POST(request, res) {
@@ -30,46 +20,19 @@ export async function POST(request, res) {
             });
     }
 
-    const emailHtml = render(<EmailTemplate name={name} message={message} />);
 
-
-    const mailOptions = {
-        from: process.env.EMAIL,
-        subject: 'Form Submission',
-        text: `
-      Name: ${name}
-      Message: ${message}
-    `,
-        html: emailHtml,
-    };
 
     try {
-        // await transporter.sendMail({ ...mailOptions, to: `rhythm.bhatia@cognozire.in, ${email}` });
-
-        await new Promise((resolve, reject) => {
-            // send mail
-            transporter.sendMail({ ...mailOptions, to: `souravvmishra@gmail.com, kushwaha@codestam.com` }, (err, info) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    console.log(info);
-                    resolve(info);
-                }
-            });
+        const data = await resend.emails.send({
+            from: 'No Reply <noreply@cognozire.in>',
+            to: ['souravvmishra@gmail.com', 'kushwaha@codestam.com'],
+            subject: 'Hello world',
+            react: EmailTemplate({ name, message }),
         });
 
-        return new NextResponse(JSON.stringify({ 'status': 'OK' }), { status: 200 });
+        return NextResponse.json(data);
     } catch (error) {
         console.log(error);
-        return new NextResponse(
-            JSON.stringify({ error: 'Internal Server Error' }),
-            { status: 500 }
-        )
+        return NextResponse.json({ error });
     }
-}
-
-
-export async function GET(request, res) {
-    return new NextResponse(JSON.stringify({ 'status': 'OK' }), { status: 200 });
 }
